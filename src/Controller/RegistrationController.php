7df2187dc9440ed331dba418cab2517bf367d63d
Robin FALCK <robin.falck57@gmail.com>
2022-08-04 13:26:45 +0200

    email verif without bundle

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
+use App\Repository\UsersRepository;
 use App\Security\UsersAuthenticator;
+use App\Service\JWTService;
+use App\Service\SendMailService;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,7 +21,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UsersAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UsersAuthenticator $authenticator, EntityManagerInterface $entityManager, SendMailService $mail, JWTService $jwt): Response
     {
         $user = new Users();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -36,6 +40,29 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
+            // JWT de l'utilisateur
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+
+            $payload = [
+                'user_id' => $user->getId()
+            ];
+
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+            $mail->send(
+                'no-reply@cours-symfony.test',
+                $user->getEmail(),
+                'Activation de votre compte sur le site e-commerce',
+                'register',
+                [
+                    'user' => $user,
+                    'token' => $token
+                ]
+            );
+
             return $userAuthenticator->authenticateUser(
                 $user,
                 $authenticator,
@@ -46,5 +73,66 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/verif/{token}', name: 'app_verify_user')]
+    public function verifyUser($token, JWTService $jwt, UsersRepository $usersRepository, EntityManagerInterface $em): Response
+    {
+        if ($jwt->isValid($token) && !$jwt->isExpired($token) && $jwt->check($token, $this->getParameter('app.jwtsecret'))) {
+            $payload = $jwt->getPayload($token);
+
+            $user = $usersRepository->find($payload['user_id']);
+
+            if ($user && !$user->getIsVerified()) {
+                $user->setIsVerified(true);
+                $em->flush($user);
+                $this->addFlash('green', 'Utilisateur activé');
+                return $this->redirectToRoute('app_profile_index');
+            }
+        }
+
+        $this->addFlash('red', 'Le token est invalide ou à expiré');
+        return $this->redirectToRoute('app_login');
+    }
+
+    #[Route('/resendverif', name: 'app_resend_verif')]
+    public function resendVerif(JWTService $jwt, SendMailService $mail, UsersRepository $usersRepository): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            $this->addFlash('red', 'Vous devez être connecté pour accéder à cette page');
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($user->getIsVerified()) {
+            $this->addFlash('orange', 'Cet utilisateur est déjà activé');
+            return $this->redirectToRoute('app_profile_index');
+        }
+
+        $header = [
+            'typ' => 'JWT',
+            'alg' => 'HS256'
+        ];
+
+        $payload = [
+            'user_id' => $user->getId()
+        ];
+
+        $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+        $mail->send(
+            'no-reply@cours-symfony.test',
+            $user->getEmail(),
+            'Activation de votre compte sur le site e-commerce',
+            'register',
+            [
+                'user' => $user,
+                'token' => $token
+            ]
+        );
+
+        $this->addFlash('green', 'Email de vérification envoyé');
+        return $this->redirectToRoute('app_profile_index');
     }
 }
